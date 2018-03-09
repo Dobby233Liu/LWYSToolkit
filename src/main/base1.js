143b1c42b1057f2a159427ab18deefb6fcb65484
Liu-Wen-Yuan <15816141883@163.com>
2018-03-09 23:56:37 +0800

    Add files via upload

--- src/main/base1.js
+++ src/main/base1.js
@@ -10,5 +10,15 @@ var possibe3=Base64!=null||MIMEType!=null
 var possibe4=Base64!=""||MIMEType!=""
 if(possibe1||possibe2||possibe3||possibe4){
 var url="data:"+MIMEType+";base64,"+Base64;
-  return url
-}return null}
+  return url;
+}return null;}
+
+/**
+* function dataUrlComp(Blob blob)
+* This is for convert the Blob to Urls
+* @author Liu wenyuan
+**/
+
+function dataUrlComp(blob){
+	return window.URL.createObjectURL(data);
+}
